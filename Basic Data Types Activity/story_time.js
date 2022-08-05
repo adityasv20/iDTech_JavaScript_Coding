c478f761768e8e0d0c40bc804b08e74cf6afe5a9
Aditya <adityasv20@gmail.com>
2022-08-05 13:11:56 -0400

    V2

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,38 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+
+var name = "Aditya Velagapudi";
+var job = "Game Developer";
+var salary = "10 million dollars";
+var city = "Silicon Valley";
+
+
+
+// TODO: Create 3-Integer variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+
+var age = 30;
+var currentYear = 2036;
+var experience = 10;
+
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+
+var gamesCreated = ["Punch Man", "Ninja Jump", "Sword Arena", "Sky Fall"];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+
+var hasMoved = true;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+
+console.log("Hello old friends, my name is " + name);
+console.log("The year is " + currentYear + " and I am " + age + " years old.");
+console.log("The rumors are " + hasMoved + ". I have moved to " + city + " to pursue my dreams!")
+console.log("The reason I had enough money to move is because of my " + salary + " salary.");
+console.log("However, this money did not come easy as I have worked " + experience + " years as a " + job + ".");
+console.log("I have made many games but my favorite is " + gamesCreated[2] + ".");
+console.log("Anyways I hope to create more games and inspire people across the world! - " + name)
